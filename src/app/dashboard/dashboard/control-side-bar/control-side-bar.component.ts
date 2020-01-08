ad75806d8df74699d5fdd270794cbae90e9f4411
Camilo Soto Montoya <csotomon@hotmail.com>
2020-01-07 23:08:17 -0500

    varios

--- src/app/dashboard/dashboard/control-side-bar/control-side-bar.component.ts
+++ src/app/dashboard/dashboard/control-side-bar/control-side-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { } from 'jquery';
 
 @Component({
   selector: 'app-dashboard-control-side-bar',
@@ -10,6 +11,39 @@ export class ControlSideBarComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const $sidebar = jQuery('.control-sidebar');
+    const $container = jQuery('<div />', {
+      class: 'p-3 control-sidebar-content'
+    });
+
+    $sidebar.append($container);
+
+    const navbar_dark_skins = [
+      'navbar-primary',
+      'navbar-secondary',
+      'navbar-info',
+      'navbar-success',
+      'navbar-danger',
+      'navbar-indigo',
+      'navbar-purple',
+      'navbar-pink',
+      'navbar-teal',
+      'navbar-cyan',
+      'navbar-dark',
+      'navbar-gray-dark',
+      'navbar-gray',
+    ];
+
+    const navbar_light_skins = [
+      'navbar-light',
+      'navbar-warning',
+      'navbar-white',
+      'navbar-orange',
+    ];
+
+    $container.append(
+      '<h5>Customize AdminLTE</h5><hr class="mb-2"/>'
+    );
   }
 
 }
