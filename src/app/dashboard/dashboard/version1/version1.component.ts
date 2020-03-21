5004ebbcadbe8da17cca2ea306168b0f3ca33d02
Camilo Soto Montoya <csotomon@hotmail.com>
2020-03-21 16:07:56 -0500

    v1

--- src/app/dashboard/dashboard/version1/version1.component.ts
+++ src/app/dashboard/dashboard/version1/version1.component.ts
@@ -76,32 +76,49 @@ export class Version1Component implements OnInit {
 
     // World map by jvectormap
     $('#world-map').vectorMap({
-      map              : 'usa_en',
-      backgroundColor  : 'transparent',
-      regionStyle      : {
+      map: 'usa_en',
+      backgroundColor: 'transparent',
+      regionStyle: {
         initial: {
-          fill            : 'rgba(255, 255, 255, 0.7)',
-          'fill-opacity'  : 1,
-          stroke          : 'rgba(0,0,0,.2)',
-          'stroke-width'  : 1,
+          fill: 'rgba(255, 255, 255, 0.7)',
+          'fill-opacity': 1,
+          stroke: 'rgba(0,0,0,.2)',
+          'stroke-width': 1,
           'stroke-opacity': 1
         }
       },
-      series           : {
+      series: {
         regions: [{
-          values           : visitorsData,
-          scale            : ['#ffffff', '#0154ad'],
+          values: visitorsData,
+          scale: ['#ffffff', '#0154ad'],
           normalizeFunction: 'polynomial'
         }]
       },
-      onRegionLabelShow ({ e, el, code }: { e; el; code; }) {
+      onRegionLabelShow({ e, el, code }: { e; el; code; }) {
         if (typeof visitorsData[code] != 'undefined') {
           el.html(el.html() + ': ' + visitorsData[code] + ' new visitors')
         }
       }
     });
 
+    /*
+    // Sparkline charts
+    var sparkline1 = new Sparkline($("#sparkline-1")[0], { width: 80, height: 50, lineColor: '#92c1dc', endColor: '#ebf4f9' });
+    var sparkline2 = new Sparkline($("#sparkline-2")[0], { width: 80, height: 50, lineColor: '#92c1dc', endColor: '#ebf4f9' });
+    var sparkline3 = new Sparkline($("#sparkline-3")[0], { width: 80, height: 50, lineColor: '#92c1dc', endColor: '#ebf4f9' });
+
+    sparkline1.draw([1000, 1200, 920, 927, 931, 1027, 819, 930, 1021]);
+    sparkline2.draw([515, 519, 520, 522, 652, 810, 370, 627, 319, 630, 921]);
+    sparkline3.draw([15, 19, 20, 22, 33, 27, 31, 27, 19, 30, 21]);
+    */
+
+    // The Calender
+    $('#calendar').datetimepicker({
+      format: 'L',
+      inline: true
+    })
 
   }
+
 
 }
