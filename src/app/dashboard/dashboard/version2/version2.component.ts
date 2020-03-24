5d0314a35dceb7077670a548ee2fb0e7aea9956e
Camilo Soto Montoya <csotomon@hotmail.com>
2020-03-24 17:35:55 -0500

    dashboards

--- src/app/dashboard/dashboard/version2/version2.component.ts
+++ src/app/dashboard/dashboard/version2/version2.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { } from 'jquery';
+
+
+// This is not a good practice but is neccesary for jquery modules what doesn't have @types
+declare let $: any;
 
 @Component({
   selector: 'app-version2',
@@ -7,9 +12,99 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Version2Component implements OnInit {
 
+  /* Chart.js Charts */
+  // Sales chart
+  public salesChartData = {
+    type: 'line',
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [
+      {
+        label: 'Digital Goods',
+        backgroundColor: 'rgba(60,141,188,0.9)',
+        borderColor: 'rgba(60,141,188,0.8)',
+        pointRadius: false,
+        pointColor: '#3b8bba',
+        pointStrokeColor: 'rgba(60,141,188,1)',
+        pointHighlightFill: '#fff',
+        pointHighlightStroke: 'rgba(60,141,188,1)',
+        data: [28, 48, 40, 19, 86, 27, 90]
+      },
+      {
+        label: 'Electronics',
+        backgroundColor: 'rgba(210, 214, 222, 1)',
+        borderColor: 'rgba(210, 214, 222, 1)',
+        pointRadius: false,
+        pointColor: 'rgba(210, 214, 222, 1)',
+        pointStrokeColor: '#c1c7d1',
+        pointHighlightFill: '#fff',
+        pointHighlightStroke: 'rgba(220,220,220,1)',
+        data: [65, 59, 80, 81, 56, 55, 40]
+      },
+    ]
+  };
+
+  public salesChartOptions = {
+    maintainAspectRatio: false,
+    responsive: true,
+    legend: {
+      display: false
+    },
+    scales: {
+      xAxes: [{
+        gridLines: {
+          display: false,
+        }
+      }],
+      yAxes: [{
+        gridLines: {
+          display: false,
+        }
+      }]
+    }
+  };
+
+  public pieData = {
+    type: 'doughnut',
+    labels: [
+      'Chrome',
+      'IE',
+      'FireFox',
+      'Safari',
+      'Opera',
+      'Navigator',
+    ],
+    datasets: [
+      {
+        data: [700, 500, 400, 600, 300, 100],
+        backgroundColor: ['#f56954', '#00a65a', '#f39c12', '#00c0ef', '#3c8dbc', '#d2d6de'],
+      }
+    ]
+  }
+  public pieOptions = {
+    legend: {
+      display: false
+    }
+  };
+
   constructor() { }
 
   ngOnInit(): void {
+
+    /* jVector Maps
+     * ------------
+     * Create a world map with markers
+     */
+    $('#world-map-markers').mapael({
+      map: {
+        name: 'usa_states',
+        zoom: {
+          enabled: true,
+          maxLevel: 10
+        },
+      },
+    }
+    );
+
   }
 
 }
